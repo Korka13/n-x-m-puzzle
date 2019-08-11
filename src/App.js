558357b2c13d49f6e92a2af85170f8f4ea029c9b
Korka13 <korka1313@gmail.com>
2019-08-11 12:00:06 +0000

    check solvability and restrict single col/row

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Board nrows={4} ncols={4} shuffle={true} />
+        <Board nrows={5} ncols={1} shuffle={true} />
       </div>
     );
   }
--- src/Board.js
+++ src/Board.js
@@ -4,8 +4,8 @@ import './Board.css';
 
 class Board extends Component {
   static defaultProps = {
-    nrows: 5,
-    ncols: 5,
+    nrows: 4,
+    ncols: 4,
     shuffle: true
   }
   constructor(props) {
@@ -22,10 +22,22 @@ class Board extends Component {
     this.animateCell = this.animateCell.bind(this);
   }
 
+  getRows(){
+    let rows;
+    this.props.nrows > 1 ? rows = this.props.nrows : rows = 2
+    return rows
+  }
+
+  getCols(){
+    let cols;
+    this.props.ncols > 1 ? cols = this.props.ncols : cols = 2
+    return cols
+  }
+
   buildSequence(){
     let sequence = [];
     let counter = 1;
-    for(let i = 1; i < this.props.nrows * this.props.ncols; i++){
+    for(let i = 1; i < this.getRows() * this.getCols(); i++){
       sequence.push(counter);
       counter++
     }
@@ -34,24 +46,67 @@ class Board extends Component {
   }
 
   createBoard() {
-    let boardValues = this.buildSequence();
+    let boardValues = this.buildSequence()
+    if(this.props.shuffle){
+      boardValues = this.shuffleSequence(boardValues)
+    }
     let board = [];
-    for (let y = 0; y < this.props.nrows; y++) {
+    for (let y = 0; y < this.getRows(); y++) {
       let row = [];
-      for (let x = 0; x < this.props.ncols; x++) {
-        if(this.props.shuffle){
-          let randIdx = Math.floor(Math.random() * boardValues.length);
-          let randCel = boardValues.splice(randIdx, 1)[0];
-          row.push(randCel);
-        }
-        else{
+      for (let x = 0; x < this.getCols(); x++) {
           let cell = boardValues.splice(0, 1)[0];
           row.push(cell);
-        }
       }
       board.push(row);
     }
     return board;
+  }
+
+  shuffleSequence(sequence){
+
+    let shuffledValues = [];
+      for(let i = 0; i < this.getRows() * this.getCols(); i ++){
+        let randIdx = Math.floor(Math.random() * sequence.length);
+        let randCel = sequence.splice(randIdx, 1)[0];
+        shuffledValues.push(randCel);
+      }
+      if(this.checkSolvability(shuffledValues)){
+        return shuffledValues
+      }else{
+        return this.shuffleSequence(shuffledValues)
+      }
+      
+  }
+
+  checkSolvability(sequence){
+    let row = 1; // start calculation from 1st row
+    let blankRow; // define blank row
+    let inversions = 0; // start from 0 inversions
+    for(let i = 0; i < sequence.length; i ++){
+      if(i % this.getCols() === 0){ // it means we are at the 1st cell of next row
+        row++
+      }
+      if(sequence[i] === null){
+        blankRow = row; //save y index of blank cell
+      }
+      for(let j = i + 1; j < sequence.length; j++){ //evaluate i against all cells are following it in the sequence
+        if(sequence[i] > sequence[j] && sequence[j] !== null){
+          inversions++
+        }
+      }
+    }
+    if (this.getCols() % 2 === 0){ //even columns configuration
+      if (this.getRows() -  blankRow % 2 === 0){ // columns - blankrow is even?
+        console.log(inversions % 2 === 0)
+        return inversions % 2 === 0
+      }else{ // columns - blankrow is odd?
+        console.log(inversions % 2 !== 0)
+        return inversions % 2 !== 0
+      }
+    }else{ // odd columns configuration
+      console.log(inversions % 2 === 0)
+      return inversions % 2 === 0
+    }
   }
 
   checkWinner(currentSequence){
@@ -73,10 +128,9 @@ class Board extends Component {
   }
 
   animateCell(y, x){
-    let {ncols, nrows} = this.props;
     const board = JSON.parse(JSON.stringify(this.state.board));
     let direction;
-    if(x + 1 < ncols && board[y][x + 1] === null){
+    if(x + 1 < this.getCols() && board[y][x + 1] === null){
       board[y][x + 1] = board[y][x];
       board[y][x] = null;
       direction = "right";
@@ -86,7 +140,7 @@ class Board extends Component {
       board[y][x] = null;
       direction = "left";
     }
-    if(y + 1 < nrows && board[y + 1][x] === null){
+    if(y + 1 < this.getRows() && board[y + 1][x] === null){
       board[y + 1][x] = board[y][x];
       board[y][x] = null;
       direction = "down";
@@ -116,8 +170,8 @@ class Board extends Component {
                         animateCell={this.animateCell} 
                         yIndex={yIndex} 
                         xIndex={xIndex} 
-                        ncols={this.props.ncols}
-                        nrows={this.props.nrows}
+                        ncols={this.getCols()}
+                        nrows={this.getRows()}
                         isMoving={this.state.movingCell.y === yIndex && this.state.movingCell.x === xIndex ? true : false}
                         direction={this.state.movingCell.y === yIndex && this.state.movingCell.x === xIndex ? this.state.movingCell.direction : null}
                       />
