58de3bfbc8f3619d379819c02be02567ea761e59
Korka13 <korka1313@gmail.com>
2019-10-31 15:14:56 +0000

    Updates

---
+++ precache-manifest.73176fd9b63f1ea393dab3da25daaf73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36ea8e0d9e1485450439",
+    "url": "/n-x-m-puzzle/static/js/runtime~main.36ea8e0d.js"
+  },
+  {
+    "revision": "94966746acf8118d7dbc",
+    "url": "/n-x-m-puzzle/static/js/main.94966746.chunk.js"
+  },
+  {
+    "revision": "9f53acdc83bf0c04cbaf",
+    "url": "/n-x-m-puzzle/static/js/1.9f53acdc.chunk.js"
+  },
+  {
+    "revision": "94966746acf8118d7dbc",
+    "url": "/n-x-m-puzzle/static/css/main.2480dc2b.chunk.css"
+  },
+  {
+    "revision": "67c86d87b51ecaadcc2e266bec21b977",
+    "url": "/n-x-m-puzzle/index.html"
+  }
+];
